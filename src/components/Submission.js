7d071f5b47f354b2a9dee909af867e2779087d38
Jessica Peng <jesspeng@Jessicas-MacBook-Pro-4.local>
2023-07-13 18:09:58 -0700

    show proper text in collapsible

--- src/components/Submission.js
+++ src/components/Submission.js
@@ -2,33 +2,32 @@ import React from 'react';
 import {getFirst150Words, getRestOfText} from "../utils/globalUtils"
 
 const Submission = ({submission, issueNumber}) => {
-
-    const displayedText = getFirst150Words(submission.text);
-    const hiddenText = getRestOfText(submission.text);
     return (
         <div>
             <h1 id="submission-title">{submission.title}</h1>
             <h2 id="submission-author">{submission.author}</h2>
 
-
             <div id="submission-text" className="row">
                 <div className="span4 collapse-group">
-                    {submission.text.split(" ").length > 150 ?
+                    {submission.id !== 2
+                        ? submission.text.split(" ").length > 150 ?
                         <>
-                            <p>{displayedText}</p>
-                            <p>
-                                <a className="read-more" data-bs-toggle="collapse" href="#collapseExample"
-                                   role="button"
-                                   aria-expanded="false" aria-controls="collapseExample">
-                                    Read more &raquo;
-                                </a>
-                            </p>
-                            <div className="collapse" id="collapseExample">
-                                <div className="card card-body">
-                                    {hiddenText}
-                                </div>
-                            </div>
-                        </> : <>{displayedText}</>}
+                        <p>{getFirst150Words(submission.text)}</p>
+                        <p>
+                        <a className="read-more" data-bs-toggle="collapse" href={"#collapse-"+submission.id}
+                        role="button"
+                        aria-expanded="false" aria-controls={"collapse-"+submission.id}>
+                        Read more &raquo;
+                        </a>
+                        </p>
+                        <div className="collapse" id={"collapse-"+submission.id}>
+                        <div className="card card-body">
+                    {getRestOfText(submission.text)}
+                        </div>
+                        </div>
+                        </> : <>{submission.text}</>
+                        : <>{submission.text}</>
+                    }
                 </div>
                 {
                     submission.images.map(image => (
--- src/data/submissions.js
+++ src/data/submissions.js
@@ -1,3 +1,39 @@
+const letterToAFriend =
+    <div>
+        <p>Heliogabalus drowned his dinner guests in flower petals.</p>
+        <p>He mixed their lentils with onyx, he mistook emeralds for spring onions&nbsp;</p>
+        <p>I have to imagine.&nbsp;</p>
+        <p>Decadent,&nbsp;they said,&nbsp;wasteful, corrupt, depraved, shameful.&nbsp;</p>
+        <p>No. I get it.&nbsp;</p>
+        <p>Couldn&rsquo;t he have just loved them so much without knowing how?</p>
+        <p>Just wanted them all to sleep over?</p>
+        <p>Just thought that choking on rose petals with all</p>
+        <p>(aren&rsquo;t we all in love with our friends) (tumultuously and wonderfully and chastely in love)</p>
+        <p>the loves of his life, thought it would be the right way, the only way to go,&nbsp;</p>
+        <p>destined by his Eastern Gods?</p>
+        <p><br/></p>
+        <p>Everyone I know is falling in or out of love.&nbsp;</p>
+        <p>Some are doing both. Some don&apos;t know they are doing either.&nbsp;</p>
+        <p>Some think they are doing one but are actually doing the other.&nbsp;</p>
+        <p>Cocky of me to think I know these things, I know.&nbsp;Shh, I&apos;ll kiss you. Don&rsquo;t worry about it. &nbsp;</p>
+        <p>I want to hold up my hands like they&rsquo;re scaling something unstable, climbing wet metal.&nbsp;</p>
+        <p>I want to catch their dead weight and cushion them with my skull, whichever way they&rsquo;re falling.&nbsp;</p>
+        <p>I&rsquo;ll take care of you I&apos;ll take care of you I&apos;ll take care of you I&apos;ll take care of you&nbsp;I don&apos;t want</p>
+        <p>to watch a heart hit the pavement, the spatter, the ooze will</p>
+        <p>occupy my intrusive thoughts for decades to come just let me</p>
+        <p>catch you.&nbsp;</p>
+        <p><br/></p>
+        <p>But even the friends for whom I would eat peonies until I suffocated&nbsp;</p>
+        <p>are going to slip through my fingers and I theirs.&nbsp;</p>
+        <p>There are unbendable rules of this reality that I can&rsquo;t fix by magic.&nbsp;</p>
+        <p>You can&rsquo;t catch what&rsquo;s falling, all you can do is beat the electricity back&nbsp;</p>
+        <p>into its ruptured atriums when it hits the ground. Don&rsquo;t touch your face with your hands.&nbsp;</p>
+        <p>Someday I&apos;ll fall in love again. They&rsquo;ll try to catch me.&nbsp;</p>
+        <p>They know my method of feeling. Tangy, viscous, gluttonous. Overbroad and overgrown.</p>
+        <p>Sticky orange pollen from lilies packing subdural space like sawdust. Bulbous moons for eyes.&nbsp;</p>
+        <p>And I&apos;ll brush their hands away like my own hair sweeping my shoulders and look&nbsp;</p>
+        <p>at some girl and give her soft soft body everything that it wants.</p>
+</div>
 export const Submissions = [
     {
         issueNumber: 0,
@@ -20,35 +56,32 @@ export const Submissions = [
                 title: "Letter to a Friend",
                 author: "Ella Helmuth",
                 columnCount: "1",
-                text: "NOW Bim was in his cell meditating upon the circumstances that loomed over him and the rest of the Vaughn17. And an Angel of the Lord appeared to him in a flame of fire from the midst of his mattress. So he looked and behold, the mattress was burning with fire, but it was not consumed. Nor did smoke materialize in the cell. Then Bim said, “Why this is a great sight, but why does the mattress not burn?” So when the Lord saw that Bim was utterly amazed, God called to him from the midst of the mattress and said, “I am God of your father—God of George Jackson—God of Marcus Garvey—God of Nat Turner.” The Lord said, “I have seen my people (the v17) who stand trial in Delaware, and have heard their cry, for I know their sorrow. So I have come down to deliver them out of the hands of Deputy Attorney General Downs. Now therefore, behold the cry of V17 has come to me, and I have also seen the oppression placed upon them by the Department of Corrections. Come now, therefore, and I will send you to the Superior Court that you may bring my people the v17 from the grips of D.A.G. Downs.” But Bim said to God, “Who am I that I should go to the Superior Court, and that I should bring the V17 out of the grasp of D.A.G Downs?” So He said, “I shall give a sign that I have sent you.” Then Bim said to God, “Indeed, when I come to the v17 and say to them, The God of your fathers has sent me to you, and they ask What’s his name?, what shall I say to them? And God said to Bim, “I am who I am.” And he said, “Thus you shall say to the v17, ‘I am’ sent me to you.” Moreover, God said to Bim “Thus you shall say to the v17: The Lord God of your fathers, the God of George Jackson, the God of Marcus Garvey, the God of Nat Turner, appeared to me saying, I have surely visited you and seen what is done to you by the Department of Corrections and D.A.G. Downs.” Then Bim answered and said, “But suppose they will not be-lieve me or listen to my voice; suppose they say The Lord has not appeared to you?” So the Lord said to him, “What is that in your hand?” And he said,\n" +
-                    "“A pen.” And He said, “Cast it on the ground.” And it became a Black Panther and Bim fled from it. Then the Lord said to Bim, “Reach out your hand and take it by the tail.” And he reached out his hand and caught it, and it became a pen again. The Lord: “They may be-lieve that the Lord God of their fathers, the God of George Jackson, the God of Marcus Garvey, and the God of Nat Turner has appeared to you.” Then Bim said to the Lord, “O my Lord I’m not a eloquent speaker, neither before or since you have spoken to your servant.” So the Lord said to him, “Who has made man’s mouth? Or who makes the mute, the deaf, the seeing, or the blind? Have not I the Lord? Now therefore, go, and I will be with your mouth and teach you what you shall say.” But he said, “O my Lord, please send by the hand of whomever else you may send.” So the anger of the Lord was kindled against Bim, and He said, “Is not Ruk of the North your brother? I know that he can speak well. You will meet him and when he sees you he will be glad in his heart.” Then Bim and Ruk went and gathered together with the rest of V17. And Ruk spoke all the words which the Lord had spoken. So the v17 believed when they heard the Lord had visited them, and when they heard that he was sympa-thetic to their affliction they bowed their heads graciously. Afterwards Bim and Ruk went to Superior Court and told D.A.G. Downs, “Thus says the Lord God of V17, Let my people go!” And D.A.G. Downs said, “Who is the lord, that I should obey his voice to let Vaughn17 go? I do not know the lord nor will I let Vaughn17 go!” As a result D.A.G. Downs conspired with the Judge to bring forth an order to conceal all evidence against Vaughn17, locked them in solitary confinement, solicited the wicked to spew false testimony against them, restricted communi-cation with their loved ones, spread vicious lies so V17 would be ostracized by the people, cast spells causing v17 to be suspicious of one another, and appointed devils disguised as lawyers to represent some of them… So Bim returned to the Lord and asked, “Lord, why have you brought trouble on these people? For since I came to D.A.G. Downs to speak in your name he has done evil to these people.” Then the Lord said to Bim, “Now you shall see what I will do to D.A.G. Downs. For with a strong hand he will let them go. I have also heard the groaning of the v17 who all face life sentences because of D.A.G. Downs. Therefore say to v17 I am the Lord, I will bring you out from the burdens of D.A.G. Downs. I will rescue you from his abuse of power, I will redeem you with an outstretched arm and with great judgements.” So Bim spoke thus to v17 but they did not heed him, for anguish of spirit, cruel bondage, and psychological torment. Then the Lord spoke to Bim and Ruk, and gave them a command for the v17 and a command for D.A.G. Downs to drop the case against v17. And said, “I will harden D.A.G. Downs’ heart so he will not heed you, this way I will lay my hand on the Superior Court and bring V17 out of the clutches of D.A.G. Downs.” Then the Lord spoke to Bim and Ruk saying, “When D.A.G. Downs speaks to you, saying Show me a miracle for yourselves, then You and Ruk shall take your pens and cast them before D.A.G. Downs, and let them become Black Panthers.” So Bim and Ruk went to D.A.G. Downs and they did so, Bim and Ruk cast down their pens before D.A.G. Downs and his servants and they became Black Panthers. But D.A.G. Downs also called his detec-tives, investigators, and witnesses, and called for them to cast down their pens and let them become serpents and pigs. The Black Panthers slaughtered all of them. One of the Panthers mauled\n" +
-                    "D.A.G. Downs, knocking him to the ground. Its eyes glared red while growl-ing into D.A.G. Downs’ sobbing face. The Lord said to Bim, “Let him live for his defeat and the liberation of v17 will be a testimony to the power of God.” Bim reluctantly grabbed the panther’s tail and it turned back into a pen.\n" +
-                    "THE END",
+                text: letterToAFriend,
                 images: [
                     {
                         url: "lettertoafriend.png",
                         alt: "Letter to a Friend",
-                        size: "small"
+                        size: "medium"
                     },
                     {
                         url: "img.png",
                         alt: "Letter to a Friend",
-                        size: "small"
+                        size: "medium"
                     },
                     {
                         url: "img_1.png",
                         alt: "Letter to a Friend",
-                        size: "small"
+                        size: "medium"
                     },
                     {
                         url: "img_2.png",
                         alt: "Letter to a Friend",
-                        size: "small"
+                        size: "medium"
                     },
                     {
                         url: "img_3.png",
                         alt: "Letter to a Friend",
-                        size: "small"
+                        size: "medium"
                     }
                 ],
                 footnote: ""
@@ -72,7 +105,7 @@ export const Submissions = [
                     {
                         url: "lares.png",
                         alt: "En el ombligo del mundo by Maria Lares",
-                        size: "small"
+                        size: "medium"
                     }
                 ],
                 footnote: ""
